fe7923b02cf904aafd4745464fcc3a72ac6d298c
Vinícius Pereira <viniciusofp@gmail.com>
2025-02-14 18:09:44 -0300

    ESLint updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,8 +15,9 @@ const eslintConfig = [
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-explicit-any': 'off'
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off'
     }
   })
 ];
